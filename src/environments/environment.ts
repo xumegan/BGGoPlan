63459fe26fd194ed9defc40f580834d81c703f05
Megan <67032904+xumegan@users.noreply.github.com>
2021-08-31 11:55:00 -0600

    Update environment.ts
     delete firebase info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyC7CCgimJxO_mZ-DDuI6nnCT0DlQBIpDTk",
-    authDomain: "bggoplan-sandbox2.firebaseapp.com",
-    databaseURL: "https://bggoplan-sandbox2-default-rtdb.firebaseio.com",
-    projectId: "bggoplan-sandbox2",
-    storageBucket: "bggoplan-sandbox2.appspot.com",
-    messagingSenderId: "685968950633",
-    appId: "1:685968950633:web:848b70ef0fdd954729e9f8",
-    measurementId: "G-TZEMCZ6LWJ"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
